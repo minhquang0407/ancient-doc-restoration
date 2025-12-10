05d366e95c337967ab23ddea22bd1f6163bb595f
Langtucodon <minhquang04072005@gmail.com>
2025-12-10 20:56:08 +0700

    Done forenstic and deskewer, close #7

--- src/core/denoiser.py
+++ src/core/denoiser.py
@@ -1,15 +1,75 @@
+import numpy as np
+
 class ImageDenoiser:
     def manual_median_filter(self, image: np.ndarray, ksize: int = 3) -> np.ndarray:
         """Tự cài đặt bộ lọc Median (Sắp xếp mảng)."""
-        pass
+        if len(image.shape) != 2:
+            raise TypeError("Image must be grayscale")
+
+        h, w = image.shape
+        output = np.zeros_like(image)
+        pad = ksize // 2
+        img_padded = np.pad(image, ((pad, pad), (pad, pad)), mode='edge')
+
+
+        for i in range(h):
+            for j in range(w):
+                window = img_padded[i:i + ksize, j:j + ksize]
+
+                sorted_pixels = np.sort(window)
+
+                output[i, j] = np.median(sorted_pixels)
+
+        return output
 
     def create_gaussian_kernel(self, ksize: int, sigma: float) -> np.ndarray:
-        """Tự tạo ma trận Gaussian Kernel."""
-        pass
+        """
+        Tự tạo ma trận Gaussian Kernel.
+        G(x,y) = (1 / 2*pi*sigma^2) * exp(-(x^2 + y^2) / 2*sigma^2)
+        Theo G ở trên thì G đạt max tại (0,0) và đó phải là tâm kernel.
+        Nhưng vì theo lập trình thì tâm ở (ksize//2,    ksize//2).
+        Do đó phải chuẩn hóa lại tọa độ để tính.
+        """
+        kernel = np.zeros((ksize,ksize), dtype= np.float32)
+        center = ksize // 2
+        sum_val = 0.0
+        for x in range(ksize):
+            for y in range(ksize):
+                # Chuẩn hóa tọa độ.
+                rel_x = x- center
+                rel_y = y- center
+
+                exponent = -(rel_x**2 + rel_y**2) / (2 * sigma**2)
+
+                val = (1 / (2 * np.pi * sigma**2)) * np.exp(exponent)
+
+                kernel[x, y] = val
+
+                sum_val += val
+
+        return kernel / sum_val
 
     def manual_convolution(self, image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
         """Tự thực hiện tích chập 2D."""
-        pass
+        if len(image.shape) != 2:
+            raise TypeError("Image must be grayscale")
+
+        ksize = kernel.shape[0]
+        pad = ksize // 2
+        h, w = image.shape
+
+        img_padded = np.pad(image, ((pad, pad), (pad, pad)), mode='constant')
+
+        output = np.zeros_like(image, dtype=np.float32)
+        for i in range(h):
+            for j in range(w):
+                region = img_padded[i:i + ksize, j:j + ksize]
+
+                value = np.sum(region * kernel)
+
+                output[i, j] = value
+
+        return np.clip(output, 0, 255).astype(np.uint8)
 
     def apply_gaussian(self, image: np.ndarray, ksize: int = 3, sigma: float = 1.0) -> np.ndarray:
         """Wrapper gọi hàm convolution hoặc GaussianBlur."""
--- src/core/forenstic.py
+++ src/core/forenstic.py
@@ -1,13 +1,72 @@
-
+import numpy as np
 class ForensicInk:
     def decorrelation_stretch(self, image):
+
         """
         Tách lớp mực phai bằng PCA (Principal Component Analysis).
         Input: Ảnh màu RGB.
         Output: Ảnh xám làm nổi bật mực.
         """
+        if image is None:
+            return None
+
+        if len(image.shape) != 3:
+            print("Warning: Decorrelation Stretch requires RGB image to separate ink layers.")
+            return image
+        h, w, c = image.shape
         # 1. Reshape ảnh thành mảng (N, 3)
+        X = image.reshape(-1, 3).astype(np.float64)
+        N = X.shape[0]
         # 2. Tính Covariance Matrix
+        mu = np.sum(X, axis=0).astype(np.float64) / N
+        X_centered = X - mu
+        cov_matrix = np.dot(X_centered.T, X_centered) / (N - 1)
         # 3. Tính Eigenvalues & Eigenvectors
+        eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+
         # 4. Chiếu dữ liệu và chuẩn hóa
-        pass
+        sigma = np.sqrt(eigenvalues)
+        sigma = np.where(sigma < 1e-6, 1e-6, sigma)  # Tránh chia 0
+        scaling_matrix = np.diag(1.0 / sigma)
+        transform_matrix = np.dot(np.dot(eigenvectors, scaling_matrix), eigenvectors.T)
+        X_transformed = np.dot(X_centered, transform_matrix)
+
+        X_final = X_transformed + mu
+        _min = np.min(X_final)
+        _max = np.max(X_final)
+        if _max - _min > 0:
+            X_final = 255 * (X_final - _min) / (_max - _min)
+
+        return X_final.reshape(h, w, c).astype(np.uint8)
+
+    def inpaint_holes(image: np.ndarray, mask: np.ndarray, iterations: int = 5) -> np.ndarray:
+        """
+        Vá lỗ thủng thủ công (Diffusion based).
+        Có thể chạy trên cả ảnh xám hoặc     ảnh màu.
+        """
+        if mask is None: return image
+
+        # Xử lý cho cả ảnh xám (2D) và màu (3D)
+        is_color = len(image.shape) == 3
+        h, w = image.shape[:2]
+
+        restored = image.copy().astype(np.float32)
+        hole_pixels = np.argwhere(mask > 0)
+
+        for _ in range(iterations):
+            for y, x in hole_pixels:
+                if y <= 0 or y >= h - 1 or x <= 0 or x >= w - 1: continue
+
+                neighbors = []
+                # Kiểm tra 4 hướng
+                coords = [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)]
+                for ny, nx in coords:
+                    if mask[ny, nx] == 0:  # Chỉ lấy pixel tốt
+                        neighbors.append(restored[ny, nx])
+
+                if len(neighbors) > 0:
+                    # Tính trung bình cộng vector (nếu màu) hoặc scalar (nếu xám)
+                    val = np.mean(neighbors, axis=0)
+                    restored[y, x] = val
+
+        return restored.astype(np.uint8)
